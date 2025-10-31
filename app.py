b541fcb45dff8210c5618072f572e455262d7123
LeonieDefourneaux <leonie.defourneaux@gmail.com>
2025-10-31 16:02:44 +0100

    veriffier grande liste

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ st.write("Merci de répondre aux questions ci-dessous :")
 with st.expander("1. Quel type d'animaux domestiques préférez-vous ?"):
     choix_domestiques = st.multiselect(
         "Choisissez une ou plusieurs options :",
-        ["Chien", "Chat", "Cochon d'Inde", "Poisson rouge", "Lapin"],
+        ["Chien", "Chat", "Cochon d'Inde", "Poisson rouge", "Lapin",""],
         placeholder="Sélectionnez..."
     )
     # Question 2
 with st.expander("2. Quels animaux de la ferme appréciez-vous le plus ?"):
     choix_ferme = st.multiselect(
         "Choisissez une ou plusieurs options :",
-        ["Vache", "Cheval", "Cochon", "Mouton", "Poule"],
+        ["Vache", "Cheval", "Cochon", "Mouton", "Poule","Oie"],
         placeholder="Sélectionnez..."
     )
     # Question 3
 with st.expander("3. Quels animaux de la savane vous fascinent le plus ?"):
-    choix_sauvages = st.multiselect(
+    choix_savane = st.multiselect(
         "Choisissez une ou plusieurs options :",
-        ["Lion", "Léopard", "Tigre", "Suricate", "Phacochère", "Hyène"],
+        ["Lion", "Léopard", "Tigre", "Eléphant", "Phacochère", "Hyène"],
         placeholder="Sélectionnez..."
     )
 with st.expander("4. Quels animaux de la forêt aimez-vous le plus ?"):
@@ -29,8 +29,13 @@ with st.expander("4. Quels animaux de la forêt aimez-vous le plus ?"):
         ["Loups", "Ours", "Renard", "Ecureuil", "Cerf", "Moufette"],
         placeholder="Sélectionnez..."
     )
+with st.expander("5. Quels animaux du désert affectionnez-vous le plus ?"):
+    choix_desert= st.multiselect(
+        "Choisissez une ou plusieurs options :",
+        ["Mygale", "Fennec", "Serpent", "Chinchilla", "Chameau", "Suricate"],
+        placeholder="Sélectionnez..."
+    )
 if st.button("Envoyer mes réponses"):
-    st.subheader("📋 Résumé de vos réponses :")
-    st.write("Animaux domestiques :", choix_domestiques)
-    st.write("Animaux de la ferme :", choix_ferme)
-    st.write("Animaux sauvages :", choix_sauvages)
+    st.subheader("Analyse de vos réponses :")
+    grande_liste = choix_domestiques + choix_ferme + choix_savane + choix_foret + choix_desert
+    st.write(grande_liste)
